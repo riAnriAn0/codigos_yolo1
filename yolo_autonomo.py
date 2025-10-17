c8b4bf838ceea1274bb492ada1f492f35b750e9d
riAnriAn0 <ml2463265@gmail.com>
2025-10-16 22:20:09 -0300

    mudanca de código

--- yolo_autonomo.py
+++ yolo_autonomo.py
@@ -8,14 +8,14 @@ ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 # Set up the camera
 picam2 = Picamera2()
-picam2.video_configuration.main.size = (640, 640)
-picam2.video_configuration.main.format = "RGB888"
-picam2.video_configuration.align()
-picam2.configure("video")
+picam2.preview_configuration.main.size = (640, 640)
+picam2.preview_configuration.main.format = "RGB888"
+picam2.preview_configuration.align()
+picam2.configure("preview")
 picam2.start()
 
 # Load YOLOv8
-model = YOLO("yolov8n_ncnn_model")
+model = YOLO("yolov8n.pt")  # Use the appropriate model file
 
 while True:
     # Capture a frame from the camera
