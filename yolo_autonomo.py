c045438acabb5473e27fa4a65842401c4c27efeb
riAnriAn0 <ml2463265@gmail.com>
2025-10-17 11:03:55 -0300

    mudanças

--- yolo_autonomo.py
+++ yolo_autonomo.py
@@ -1,7 +1,9 @@
 import cv2
 from PIL import ImageFile
 from picamera2 import Picamera2
+import time
 from ultralytics import YOLO
+
 
 # Permite imagens incompletas (workaround)
 ImageFile.LOAD_TRUNCATED_IMAGES = True
@@ -15,37 +17,26 @@ picam2.configure("preview")
 picam2.start()
 
 # Load YOLOv8
-model = YOLO("yolov5n.pt")  # Use the appropriate model file
+model = YOLO("yolov8n.pt")  # Use the appropriate model file
+
 
 while True:
-    # Capture a frame from the camera
     frame = picam2.capture_array()
-    print(f"Frame shape: {frame.shape}, dtype: {frame.dtype}")  # Debug
-
-    # Corrige possível leitura invertida de cores
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    frame = cv2.resize(frame, (320, 320))
 
-    # Redimensiona para evitar problemas
-    frame = cv2.resize(frame, (640, 640))
+    results = model(frame, 0, verbose=False)
 
-    # Run YOLO model
-    results = model(frame)
-
-    # Plot results
-    annotated_frame = results[0].plot()
     inference_time = results[0].speed['inference']
-    fps = 1000 / inference_time
-    text = f'FPS: {fps:.1f}'
+    fps = 1000 / inference_time if inference_time > 0 else 0
 
-    font = cv2.FONT_HERSHEY_SIMPLEX
-    text_size = cv2.getTextSize(text, font, 1, 2)[0]
-    text_x = annotated_frame.shape[1] - text_size[0] - 10
-    text_y = text_size[1] + 10
-    cv2.putText(annotated_frame, text, (text_x, text_y), font, 1, (255, 255, 255), 2, cv2.LINE_AA)
+    print(f"FPS: {fps:.1f}")
 
-    # Display
-    cv2.imshow("Camera", annotated_frame)
-    if cv2.waitKey(1) == ord("q"):
-        break
+    for box in results[0].boxes:
+        cls = int(box.cls)
+        conf = float(box.conf)
+        label = model.names[cls]
+        print(f" - {label}: {conf:.2f}")
 
-cv2.destroyAllWindows()
+    print("-" * 30)
+    time.sleep(0.2)
